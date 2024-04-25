415786835a7695ae9809ba3d53dd6cd88f892478
Abdul Harits Iftikar Nasution <haritsnasution123@gmail.com>
2024-04-25 10:25:03 +0700

    Add files via upload

---
+++ plane.py
@@ -0,0 +1,417 @@
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import imgaug as ia
+import imgaug.augmenters as iaa
+import skimage.draw
+import matplotlib.pyplot as plt
+import time
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask as maskUtils
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath(".")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+from mrcnn import visualize
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+class_names = ['BG', 'pesawat', 'ekor', 'pintu', 'sayap', 'mesin']
+
+############################################################
+#  Configurations
+############################################################
+
+
+class PlaneConfig(Config):
+    """Configuration for training on the plane dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "plane"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 5  # Background + pesawat, ekor, pintu, sayap, mesin
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+    
+
+############################################################
+#  Dataset
+############################################################
+
+class PlaneDataset(utils.Dataset):
+
+    def load_plane(self, dataset_dir, subset):
+        """Load a subset of the Plane dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes
+        self.add_class("plane", 1, "pesawat")
+        self.add_class("plane", 2, "ekor")
+        self.add_class("plane", 3, "pintu")
+        self.add_class("plane", 4, "sayap")
+        self.add_class("plane", 5, "mesin")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val", "test"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Load annotations
+        # VGG Image Annotator (up to version 1.6) saves each image in the form:
+        # { 'filename': '28503151_5b5b7ec140_b.jpg',
+        #   'regions': {
+        #       '0': {
+        #           'region_attributes': {},
+        #           'shape_attributes': {
+        #               'all_points_x': [...],
+        #               'all_points_y': [...],
+        #               'name': 'polygon'}},
+        #       ... more regions ...
+        #   },
+        #   'size': 100202
+        # }
+        # We mostly care about the x and y coordinates of each region
+        # Note: In VIA 2.0, regions was changed from a dict to a list.
+        annotations = json.load(open(os.path.join(dataset_dir, f"{subset}VGG.json")))
+        annotations = list(annotations.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+                objects = [s['region_attributes']['label'] for s in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']]
+                objects = [s['region_attributes']['label'] for s in a['regions']]
+            name_dict = {"pesawat":1,"ekor":2,"pintu":3,"sayap":4,"mesin":5}
+            num_ids = [name_dict[a] for a in objects]
+
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "plane",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons,
+                num_ids=num_ids)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a plane dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "plane":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        if info["source"] != "plane":
+            return super(self.__class__, self).load_mask(image_id)
+        num_ids = info["num_ids"]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        num_ids = np.array(num_ids, dtype=np.int32)
+        return mask, num_ids
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "plane":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+############################################################
+#  Train
+############################################################
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = PlaneDataset()
+    dataset_train.load_plane(args.dataset, "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = PlaneDataset()
+    dataset_val.load_plane(args.dataset, "val")
+    dataset_val.prepare()
+
+    # Augmentations
+    # Sometimes(0.5, ...) applies the given augmenter in 50% of all cases,
+    # e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second image.
+    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+
+    seq = iaa.Sequential(
+    [
+            # apply the following augmenters to most images
+            iaa.Fliplr(0.5), # horizontally flip 50% of all images
+            
+            sometimes(iaa.Affine(
+                scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
+                translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+                rotate=(-45, 45), # rotate by -45 to +45 degrees
+                shear=(-16, 16), # shear by -16 to +16 degrees
+                order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+                cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+                mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+            )),
+            # execute 0 to 5 of the following (less important) augmenters per image
+            # don't execute all of them, as that would often be way too strong
+            iaa.SomeOf((0, 5),
+                [
+                    sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                    iaa.OneOf([
+                        iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                        iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                        iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                    ]),
+                    iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                    iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+
+                    # search either for all edges or for directed edges,
+                    # blend the result with the original image using a blobby mask
+
+                    iaa.SimplexNoiseAlpha(iaa.OneOf([
+                        iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                        iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                    ])),
+                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
+                    iaa.OneOf([
+                        iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                        iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                    ]),
+                    iaa.Invert(0.05, per_channel=True), # invert color channels
+                    iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+                    iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
+
+                    # either change the brightness of the whole image (sometimes
+                    # per channel) or change the brightness of subareas
+
+                    iaa.OneOf([
+                        iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                        iaa.FrequencyNoiseAlpha(
+                            exponent=(-4, 0),
+                            first=iaa.Multiply((0.5, 1.5), per_channel=True),
+                            second=iaa.ContrastNormalization((0.5, 2.0))
+                        )
+                    ]),
+                    iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
+                    iaa.Grayscale(alpha=(0.0, 1.0)),
+                    sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                    sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+                    sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+                ],
+                random_order=True
+            )
+        ],
+        random_order=True
+    )
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=300,
+                augmentation=seq,
+                layers='heads')
+
+    # *** This training schedule is an example. Update to your needs ***
+
+    # Training - Stage 1
+    # print("Training network heads")
+    # model.train(dataset_train, dataset_val,
+    #             learning_rate=config.LEARNING_RATE,
+    #             epochs=40,
+    #             layers='heads',
+    #             augmentation=seq)
+
+    # Training - Stage 2
+    # Finetune layers from ResNet stage 4 and up
+    # print("Fine tune Resnet stage 4 and up")
+    # model.train(dataset_train, dataset_val,
+    #             learning_rate=config.LEARNING_RATE,
+    #             epochs=120,
+    #             layers='4+',
+    #             augmentation=seq)
+
+    # Training - Stage 3
+    # Fine tune all layers
+    # print("Fine tune all layers")
+    # model.train(dataset_train, dataset_val,
+    #             learning_rate=config.LEARNING_RATE / 10,
+    #             epochs=160,
+    #             layers='all',
+    #             augmentation=seq)
+
+############################################################
+#  Inference
+############################################################
+
+def inference(model, image_path=None):
+    assert image_path
+    
+    # Run model detection and generate the color splash effect
+    print("Running on {}".format(args.image))
+    # Read image
+    image = skimage.io.imread(args.image)
+    # Detect objects
+    r = model.detect([image], verbose=1)[0]
+    
+    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                            class_names, r['scores'])
+    # Save output
+    # file_name = "detect_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+    # plt.savefig(file_name,bbox_inches='tight', pad_inches=-0.5,orientation= 'landscape')
+
+    # print("Saved to ", file_name)
+
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect planes.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'inference'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/plane/dataset/",
+                        help='Directory of the Plane dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--image', required=False,
+                        metavar="path or URL to image",
+                        help='Image to apply the model on')
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+    elif args.command == "inference":
+        assert args.image,\
+            "Provide --image to apply model"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = PlaneConfig()
+    else:
+        class InferenceConfig(PlaneConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+            ############################### CHANGE HYPERPARAMETERS HERE ##################
+            RPN_NMS_THRESHOLD = 0.01
+            DETECTION_MIN_CONFIDENCE = 0.7
+            DETECTION_NMS_THRESHOLD = 0.7
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model)
+    elif args.command == "inference":
+        inference(model, image_path=args.image)
+    else:
+        print("'{}' is not recognized. "
+            "Use 'train' or 'splash'".format(args.command))
